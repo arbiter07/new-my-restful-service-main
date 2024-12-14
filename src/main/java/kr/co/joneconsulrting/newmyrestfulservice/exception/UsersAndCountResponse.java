f4b5d1d8d0f29c8b2af5659e9178f2a284ba2e15
arbiter <arbiter08@naver.com>
2024-12-14 16:39:03 +0900

    first commit

---
+++ src/main/java/kr/co/joneconsulrting/newmyrestfulservice/exception/UsersAndCountResponse.java
@@ -0,0 +1,16 @@
+package kr.co.joneconsulrting.newmyrestfulservice.exception;
+
+import kr.co.joneconsulrting.newmyrestfulservice.bean.User;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@AllArgsConstructor
+@ToString
+//@Data
+//@Builder
+public class UsersAndCountResponse {
+    private int count;
+    private List<User> users;
+}
